6394ef43ed2336597b2476320ff35c6f4e3819fb
Armaan Qureshi <armaan786lucky@gmail.com>
2026-02-13 22:44:56 +0530

    Add files via upload

---
+++ task2_math_module.py
@@ -0,0 +1,14 @@
+import math    # importing built-in math module
+
+# taking input from user
+num = float(input("Enter a number:"))
+
+# calculation using math module 
+square_root = math.sqrt(num)   # square root
+logarithm = math.log(num)      # logarithm
+sine = math.sin(num)           # sine 
+
+# displaying the output 
+print(f"square root of {num} is {square_root}")
+print(f"logarithm value of {num} is {logarithm}")
+print(f"sine value of {num} is {sine}")
